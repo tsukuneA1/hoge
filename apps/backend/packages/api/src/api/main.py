aa98381a797472819c3eaf118010a0822acccc9b
tsukuneA1 <tkschr1125@gmail.com>
2026-06-26 18:24:34 +0900

    add lifespan

--- apps/backend/packages/api/src/api/main.py
+++ apps/backend/packages/api/src/api/main.py
@@ -1,7 +1,26 @@
+from contextlib import contextmanager
+
+from api.infrastructure.db.database import close_engine, init_engine
 from fastapi import FastAPI
 from libs.logging import configure_logging
 
 from api.api import courses
+
+
+@contextmanager
+def lifespan(app: FastAPI):
+    """Application lifespan manager"""
+    # Startup
+    try:
+        init_engine()
+    except Exception:
+        close_engine()
+        raise
+
+    yield
+
+    # shutdown
+    close_engine()
 
 
 def create_app() -> FastAPI:
@@ -9,6 +28,7 @@ def create_app() -> FastAPI:
     app = FastAPI(
         title="Backend API",
         version="0.1.0",
+        lifespan=lifespan
     )
 
     app.include_router(courses.router)
