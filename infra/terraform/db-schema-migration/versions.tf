141e591e1dd4b437d2773a7dcda26b51f3880ba9
tsukuneA1 <tkschr1125@gmail.com>
2026-06-30 14:24:00 +0900

    feat(infra): migrate local tfstate to gcs

--- infra/terraform/db-schema-migration/versions.tf
+++ infra/terraform/db-schema-migration/versions.tf
@@ -1,6 +1,11 @@
 terraform {
   required_version = ">=1.8.0"
 
+  backend "gcs" {
+    bucket = "waseda-syllabus-project-terraform-state"
+    prefix = "db-schema-migration"
+  }
+
   required_providers {
     google = {
         source = "hashicorp/google"
